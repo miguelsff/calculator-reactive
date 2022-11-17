9cb67200a8140edc12b4dc31336dab1baa70dff6
Miguel Concepción <miguelsff@gmail.com>
2022-11-17 13:32:50 -0500

    Spring boot init

---
+++ src/main/java/com/example/calculatorreactive/CalculatorReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.calculatorreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorReactiveApplication.class, args);
+	}
+
+}
